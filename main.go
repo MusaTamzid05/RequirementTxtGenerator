ee575db63306622a3e5130040ae71e0ae93d797a
MusaTamzid05 <tamzid101@gmail.com>
2020-08-09 04:50:12 +0600

    flag added.

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"requirements_text_generator/libs"
 )
 
 func main() {
 
+	pathPtr := flag.String("path", "", "Path to python project")
+	flag.Parse()
+
+	if *pathPtr == "" {
+		fmt.Println("Usage: python_project_path")
+		return
+	}
+
 	librarySearch := libs.LibrarySearcher{}
-	librarySearch.Search("/home/musa/git_clones/cp-vton-plus")
+	librarySearch.Search(*pathPtr)
 }
